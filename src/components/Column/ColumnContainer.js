81ef4b5e803d08a926aa603de4a9f098eb31ce7d
Bartosz Gromada <bartosz.k.gromada@gmail.com>
2020-01-08 12:14:14 +0100

    fix_bug

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -5,7 +5,7 @@ import Column from './Column.js';
 export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
 
 const mapStateToProps = (state, props) => ({
-  columns: getCardsForColumn(state, props.id),
+  cards: getCardsForColumn(state, props.id),
 });
 
 export default connect(mapStateToProps)(Column);
